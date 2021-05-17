60a958a7c216297162c36518e0b9061fda95f27f
Yvonne <yvonnersz@gmail.com>
2021-05-17 08:23:26 -0700

    Initializr

---
+++ src/test/java/com/galvanize/coffeeapi/CoffeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.coffeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
