992706a1d4664ebac0df3c08a77340c18996b670
Yvonne <yvonnersz@gmail.com>
2021-06-08 02:41:13 -0700

    Rearrange @CrossOrigin

--- src/main/java/com/galvanize/coffeeapi/CoffeeController.java
+++ src/main/java/com/galvanize/coffeeapi/CoffeeController.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @CrossOrigin
+@RestController
 public class CoffeeController {
     private CoffeeService coffeeService;
 
